8edb29c77cf810ac5bdbd2deddb6abe2f21e0c1f
LeeJinWoo <jionu102@naver.com>
2024-11-18 14:44:49 +0900

    message:

--- src/main/java/hmart/customer/CustomerApplication.java
+++ src/main/java/hmart/customer/CustomerApplication.java
@@ -13,4 +13,9 @@ public class CustomerApplication {
 }
 
 //git remote -v : 연결된 깃허브 리포지토리 확인
-//git remote remove
+//git remote remove origin : 현재 연결된 깃허브 리포지토리 삭제
+//git init : 깃 설정
+//git remote add origin <깃허브 리포지토리 주소.git>
+//git add . : 현재 모든 파일을 origin리포지토리에 추가
+//git commit -m "커밋 메시지" : 변경된 리포지토리를 확정
+//git push -u origin master : 깃 리포지토리에 푸쉬
